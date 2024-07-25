6d27289fab06cd8763fd151cf849cb8b0105185a
Erica Ford <erica.ford0205@gmail.com>
2024-07-25 13:08:00 -0500

    Test number of spaces moved logic

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,13 @@
 # Create a function that uses the Caesar Cipher technique to encrypt a string
 
 def caesar_cipher(str, shift)
+  num_spaces = 0
 
+  # Shift left making sure that if the number exceeds 25 it will shift by the correct number of spaces
+  shift.abs > 25 ? num_spaces = shift.abs % 25 : num_spaces = shift.abs
+  
+  puts num_spaces
 end
+
+# Test result
+caesar_cipher("", 55)
